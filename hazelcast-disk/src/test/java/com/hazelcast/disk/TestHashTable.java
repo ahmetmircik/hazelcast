ac5c82c46324f4658a7c4aff3e49287ddcebd4f2
ahmetmircik <ahmet@hazelcast.com>
2014-01-14 10:24:36 +0200

    refactoring

---
+++ hazelcast-disk/src/test/java/com/hazelcast/disk/TestHashTable.java
@@ -0,0 +1,14 @@
+//package com.hazelcast.disk;
+//
+//import org.junit.experimental.categories.Category;
+//import org.junit.runner.RunWith;
+//
+///**
+// * @author: ahmetmircik
+// * Date: 1/14/14
+// */
+//
+//@RunWith(HazelcastParallelClassRunner.class)
+//@Category(QuickTest.class)
+//public class TestHashTable extends HazelcastTestSupport{
+//}
