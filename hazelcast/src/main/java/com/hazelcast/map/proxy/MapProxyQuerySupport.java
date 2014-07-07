878fa8936248049f41ae909249fe4e0c7a0d54c1
ahmetmircik <ahmet@hazelcast.com>
2014-07-07 13:06:31 +0300

    map proxy package check-style compliance start

---
+++ hazelcast/src/main/java/com/hazelcast/map/proxy/MapProxyQuerySupport.java
@@ -0,0 +1,309 @@
+package com.hazelcast.map.proxy;
+
+import com.hazelcast.instance.MemberImpl;
+import com.hazelcast.map.QueryResult;
+import com.hazelcast.map.operation.QueryOperation;
+import com.hazelcast.map.operation.QueryPartitionOperation;
+import com.hazelcast.nio.serialization.SerializationService;
+import com.hazelcast.query.PagingPredicate;
+import com.hazelcast.query.PagingPredicateAccessor;
+import com.hazelcast.query.Predicate;
+import com.hazelcast.query.impl.QueryResultEntry;
+import com.hazelcast.spi.NodeEngine;
+import com.hazelcast.spi.OperationService;
+import com.hazelcast.util.ExceptionUtil;
+import com.hazelcast.util.IterationType;
+import com.hazelcast.util.QueryResultSet;
+import com.hazelcast.util.SortedQueryResultSet;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static com.hazelcast.map.MapService.SERVICE_NAME;
+
+/**
+ * Support methods for query operations in {@link com.hazelcast.map.proxy.MapProxySupport}.
+ */
+public class MapProxyQuerySupport {
+
+    private String name;
+    private NodeEngine nodeEngine;
+
+    public MapProxyQuerySupport(String name, NodeEngine nodeEngine) {
+        this.name = name;
+        this.nodeEngine = nodeEngine;
+    }
+
+
+    /**
+     * Used for paging predicate queries on node local entries.
+     *
+     * @param pagingPredicate to query.
+     * @param iterationType   type of {@link IterationType}
+     * @return {@link SortedQueryResultSet}
+     */
+    public Set queryLocalPagingPredicate(final PagingPredicate pagingPredicate,
+                                         final IterationType iterationType) {
+        final NodeEngine nodeEngine = this.nodeEngine;
+        List<Integer> partitionIds = nodeEngine.getPartitionService().getMemberPartitions(nodeEngine.getThisAddress());
+        pagingPredicate.setIterationType(iterationType);
+        if (pagingPredicate.getPage() > 0 && pagingPredicate.getAnchor() == null) {
+            pagingPredicate.previousPage();
+            queryLocalPagingPredicate(pagingPredicate, iterationType);
+            pagingPredicate.nextPage();
+        }
+        final Set result = new SortedQueryResultSet(pagingPredicate.getComparator(),
+                iterationType, pagingPredicate.getPageSize());
+        List<Integer> returnedPartitionIds;
+        try {
+            final Future future = queryLocalIndexes(pagingPredicate, nodeEngine);
+            final List<Future> singletonList = Collections.singletonList(future);
+            returnedPartitionIds = addPagingPredicateResults(singletonList, result);
+            if (returnedPartitionIds.size() == partitionIds.size()) {
+                PagingPredicateAccessor.setPagingPredicateAnchor(pagingPredicate,
+                        ((SortedQueryResultSet) result).last());
+                return result;
+            }
+            removeReturnedPartitionIdsFromWholeList(returnedPartitionIds, partitionIds);
+
+            final List<Future> futures = queryOnPartitions(pagingPredicate, partitionIds, nodeEngine);
+            addPagingPredicateResults(futures, result);
+
+        } catch (Throwable t) {
+            throw ExceptionUtil.rethrow(t);
+        }
+        return result;
+    }
+
+    /**
+     * Used for predicates which queries on node local entries, except paging predicate.
+     *
+     * @param predicate     except paging predicate.
+     * @param iterationType type of {@link IterationType}
+     * @param dataResult    <code>true</code> if results should contain {@link com.hazelcast.nio.serialization.Data} types,
+     *                      <code>false</code> for object types.
+     * @return {@link QueryResultSet}
+     */
+    public Set queryLocalPredicate(final Predicate predicate, final IterationType iterationType, final boolean dataResult) {
+        final NodeEngine nodeEngine = this.nodeEngine;
+        final List<Integer> partitionIds = nodeEngine.getPartitionService().getMemberPartitions(nodeEngine.getThisAddress());
+        final SerializationService serializationService = nodeEngine.getSerializationService();
+        final Set result = new QueryResultSet(serializationService, iterationType, dataResult);
+        List<Integer> returnedPartitionIds;
+        try {
+            final Future future = queryLocalIndexes(predicate, nodeEngine);
+            final List<Future> singletonList = Collections.singletonList(future);
+            returnedPartitionIds = addPredicateResults(singletonList, result);
+            if (returnedPartitionIds.size() == partitionIds.size()) {
+                return result;
+            }
+            removeReturnedPartitionIdsFromWholeList(returnedPartitionIds, partitionIds);
+            List<Future> futures = queryOnPartitions(predicate, partitionIds, nodeEngine);
+            addPredicateResults(futures, result);
+        } catch (Throwable t) {
+            throw ExceptionUtil.rethrow(t);
+        }
+        return result;
+    }
+
+    /**
+     * Used for paging predicate queries on all members.
+     *
+     * @param pagingPredicate to query.
+     * @param iterationType   type of {@link IterationType}
+     * @return {@link SortedQueryResultSet}
+     */
+    public Set queryPagingPredicate(PagingPredicate pagingPredicate, final IterationType iterationType) {
+        final NodeEngine nodeEngine = this.nodeEngine;
+        final Collection<MemberImpl> members = nodeEngine.getClusterService().getMemberList();
+        final int partitionCount = nodeEngine.getPartitionService().getPartitionCount();
+        List<Integer> partitionIds = getPopulatedPartitionIds(partitionCount);
+        pagingPredicate.setIterationType(iterationType);
+        if (pagingPredicate.getPage() > 0 && pagingPredicate.getAnchor() == null) {
+            pagingPredicate.previousPage();
+            queryPagingPredicate(pagingPredicate, iterationType);
+            pagingPredicate.nextPage();
+        }
+        final Set result = new SortedQueryResultSet(pagingPredicate.getComparator(),
+                iterationType, pagingPredicate.getPageSize());
+        try {
+            List<Future> futures = queryIndexes(pagingPredicate, members, nodeEngine);
+            final List<Integer> returnedPartitionIds = addPagingPredicateResults(futures, result);
+            if (partitionIds.size() == partitionCount) {
+                PagingPredicateAccessor.setPagingPredicateAnchor(pagingPredicate, ((SortedQueryResultSet) result).last());
+                return result;
+            }
+            removeReturnedPartitionIdsFromWholeList(returnedPartitionIds, partitionIds);
+        } catch (Throwable t) {
+            partitionIds = getPopulatedPartitionIds(partitionCount);
+            nodeEngine.getLogger(getClass()).warning("Exception while querying indexes ", t);
+        }
+
+        try {
+            final List<Future> futures = queryOnPartitions(pagingPredicate, partitionIds, nodeEngine);
+            addPagingPredicateResults(futures, result);
+        } catch (Throwable t) {
+            throw ExceptionUtil.rethrow(t);
+        }
+        PagingPredicateAccessor.setPagingPredicateAnchor(pagingPredicate, ((SortedQueryResultSet) result).last());
+        return result;
+    }
+
+    /**
+     * Used for predicates which queries on all members, except paging predicate.
+     *
+     * @param predicate     except paging predicate.
+     * @param iterationType type of {@link IterationType}
+     * @param dataResult    <code>true</code> if results should contain {@link com.hazelcast.nio.serialization.Data} types,
+     *                      <code>false</code> for object types.
+     * @return {@link QueryResultSet}
+     */
+    public Set queryPredicate(final Predicate predicate,
+                              final IterationType iterationType, final boolean dataResult) {
+        final NodeEngine nodeEngine = this.nodeEngine;
+        final SerializationService serializationService = nodeEngine.getSerializationService();
+        final Collection<MemberImpl> members = nodeEngine.getClusterService().getMemberList();
+        final int partitionCount = nodeEngine.getPartitionService().getPartitionCount();
+        List<Integer> partitionIds = getPopulatedPartitionIds(partitionCount);
+        final Set result = new QueryResultSet(serializationService, iterationType, dataResult);
+        try {
+            final List<Future> futures = queryIndexes(predicate, members, nodeEngine);
+            final List<Integer> returnedPartitionIdList = addPredicateResults(futures, result);
+            if (partitionIds.size() == partitionCount) {
+                return result;
+            }
+            removeReturnedPartitionIdsFromWholeList(returnedPartitionIdList, partitionIds);
+        } catch (Throwable t) {
+            partitionIds = getPopulatedPartitionIds(partitionCount);
+            nodeEngine.getLogger(getClass()).warning("Exception while querying indexes ", t);
+        }
+
+        try {
+            final List<Future> futures = queryOnPartitions(predicate, partitionIds, nodeEngine);
+            addPredicateResults(futures, result);
+        } catch (Throwable t) {
+            throw ExceptionUtil.rethrow(t);
+        }
+        return result;
+    }
+
+
+    private Future queryLocalIndexes(Predicate predicate, NodeEngine nodeEngine) {
+        final OperationService operationService = nodeEngine.getOperationService();
+        final Future future = operationService
+                .invokeOnTarget(SERVICE_NAME,
+                        new QueryOperation(name, predicate),
+                        nodeEngine.getThisAddress());
+        return future;
+    }
+
+    private List<Future> queryIndexes(Predicate predicate,
+                                      Collection<MemberImpl> members, NodeEngine nodeEngine) {
+        final List<Future> futures = new ArrayList<Future>(members.size());
+        final OperationService operationService = nodeEngine.getOperationService();
+        for (MemberImpl member : members) {
+            Future future = operationService
+                    .invokeOnTarget(SERVICE_NAME, new QueryOperation(name, predicate), member.getAddress());
+            futures.add(future);
+        }
+        return futures;
+    }
+
+
+    private void removeReturnedPartitionIdsFromWholeList(List<Integer> returnedPartitionIds,
+                                                         Collection<Integer> allPartitionIds) {
+        final Iterator<Integer> iterator = allPartitionIds.iterator();
+        while (iterator.hasNext()) {
+            final Integer partitionId = iterator.next();
+            if (returnedPartitionIds.contains(partitionId)) {
+                iterator.remove();
+            }
+        }
+    }
+
+
+    private List<Future> queryOnPartitions(Predicate predicate, List<Integer> partitionIds,
+                                           NodeEngine nodeEngine) {
+        if (partitionIds == null || partitionIds.isEmpty()) {
+            return Collections.emptyList();
+        }
+        final OperationService operationService = nodeEngine.getOperationService();
+        final List<Future> futures = new ArrayList<Future>(partitionIds.size());
+        for (Integer partitionId : partitionIds) {
+            QueryPartitionOperation queryPartitionOperation = new QueryPartitionOperation(name, predicate);
+            queryPartitionOperation.setPartitionId(partitionId);
+            try {
+                Future f =
+                        operationService.invokeOnPartition(SERVICE_NAME, queryPartitionOperation, partitionId);
+                futures.add(f);
+            } catch (Throwable t) {
+                throw ExceptionUtil.rethrow(t);
+            }
+        }
+        return futures;
+    }
+
+
+    /**
+     * For paging predicates.
+     * Adds results to result set and returns queried partition ids.
+     */
+    private List<Integer> addPagingPredicateResults(List<Future> futures, Set result)
+            throws ExecutionException, InterruptedException {
+        List<Integer> partitionIdList = Collections.emptyList();
+        for (Future future : futures) {
+            QueryResult queryResult = (QueryResult) future.get();
+            if (queryResult != null) {
+                partitionIdList = queryResult.getPartitionIds();
+                if (partitionIdList != null) {
+                    for (QueryResultEntry queryResultEntry : queryResult.getResult()) {
+                        Object key = toObject(queryResultEntry.getKeyData());
+                        Object value = toObject(queryResultEntry.getValueData());
+                        result.add(new AbstractMap.SimpleImmutableEntry(key, value));
+                    }
+                }
+            }
+        }
+        return partitionIdList;
+    }
+
+    /**
+     * For predicates except paging predicates.
+     * Adds results to result set and returns queried partition ids.
+     */
+    private List<Integer> addPredicateResults(List<Future> futures, Set result)
+            throws ExecutionException, InterruptedException {
+        List<Integer> partitionIdList = Collections.emptyList();
+        for (Future future : futures) {
+            QueryResult queryResult = (QueryResult) future.get();
+            if (queryResult != null) {
+                partitionIdList = queryResult.getPartitionIds();
+                if (partitionIdList != null) {
+                    result.addAll(queryResult.getResult());
+                }
+            }
+        }
+        return partitionIdList;
+    }
+
+    private List<Integer> getPopulatedPartitionIds(int partitionCount) {
+        final List<Integer> partitionIds = new ArrayList<Integer>(partitionCount);
+        for (int i = 0; i < partitionCount; i++) {
+            partitionIds.add(i);
+        }
+        return partitionIds;
+    }
+
+
+    private Object toObject(Object obj) {
+        return nodeEngine.getSerializationService().toObject(obj);
+    }
+
+}
