15ef970680e388007b78b7b54f99b34740d75eec
ahmetmircik <ahmet@hazelcast.com>
2014-07-07 23:51:29 +0300

    cleanup & revert old code

--- hazelcast/src/main/java/com/hazelcast/map/proxy/MapProxyQuerySupport.java
+++ hazelcast/src/main/java/com/hazelcast/map/proxy/MapProxyQuerySupport.java
@@ -158,7 +158,7 @@ public class MapProxyQuerySupport {
      *                      <code>false</code> for object types.
      * @return {@link QueryResultSet}
      */
-    public Set query(final Predicate predicate,
+    public Set query2(final Predicate predicate,
                      final IterationType iterationType, final boolean dataResult) {
         final NodeEngine nodeEngine = this.nodeEngine;
         final SerializationService serializationService = nodeEngine.getSerializationService();
@@ -254,7 +254,6 @@ public class MapProxyQuerySupport {
             final List<Integer> tmpPartitionIds = queryResult.getPartitionIds();
             if (tmpPartitionIds != null) {
                 partitionIds.removeAll(tmpPartitionIds);
-
                 result.addAll(queryResult.getResult());
             }
         }
@@ -283,29 +282,14 @@ public class MapProxyQuerySupport {
         return nodeEngine.getSerializationService().toObject(obj);
     }
 
-    protected Set queryx(final Predicate predicate, final IterationType iterationType, final boolean dataResult) {
+    protected Set query(final Predicate predicate, final IterationType iterationType, final boolean dataResult) {
         final NodeEngine nodeEngine = this.nodeEngine;
         OperationService operationService = nodeEngine.getOperationService();
         final SerializationService ss = nodeEngine.getSerializationService();
         Collection<MemberImpl> members = nodeEngine.getClusterService().getMemberList();
         int partitionCount = nodeEngine.getPartitionService().getPartitionCount();
         Set<Integer> plist = new HashSet<Integer>(partitionCount);
-        PagingPredicate pagingPredicate = null;
-        if (predicate instanceof PagingPredicate) {
-            pagingPredicate = (PagingPredicate) predicate;
-            pagingPredicate.setIterationType(iterationType);
-            if (pagingPredicate.getPage() > 0 && pagingPredicate.getAnchor() == null) {
-                pagingPredicate.previousPage();
-                query(pagingPredicate, iterationType, dataResult);
-                pagingPredicate.nextPage();
-            }
-        }
-        Set result;
-        if (pagingPredicate == null) {
-            result = new QueryResultSet(ss, iterationType, dataResult);
-        } else {
-            result = new SortedQueryResultSet(pagingPredicate.getComparator(), iterationType, pagingPredicate.getPageSize());
-        }
+        Set result = new QueryResultSet(ss, iterationType, dataResult);
         List<Integer> missingList = new ArrayList<Integer>();
         try {
             List<Future> flist = new ArrayList<Future>();
@@ -320,22 +304,11 @@ public class MapProxyQuerySupport {
                     final List<Integer> partitionIds = queryResult.getPartitionIds();
                     if (partitionIds != null) {
                         plist.addAll(partitionIds);
-                        if (pagingPredicate == null) {
-                            result.addAll(queryResult.getResult());
-                        } else {
-                            for (QueryResultEntry queryResultEntry : queryResult.getResult()) {
-                                Object key = ss.toObject(queryResultEntry.getKeyData());
-                                Object value = ss.toObject(queryResultEntry.getValueData());
-                                result.add(new AbstractMap.SimpleImmutableEntry(key, value));
-                            }
-                        }
+                        result.addAll(queryResult.getResult());
                     }
                 }
             }
             if (plist.size() == partitionCount) {
-                if (pagingPredicate != null) {
-                    PagingPredicateAccessor.setPagingPredicateAnchor(pagingPredicate, ((SortedQueryResultSet) result).last());
-                }
                 return result;
             }
             for (int i = 0; i < partitionCount; i++) {
@@ -367,21 +340,10 @@ public class MapProxyQuerySupport {
             }
             for (Future future : futures) {
                 QueryResult queryResult = (QueryResult) future.get();
-                if (pagingPredicate == null) {
-                    result.addAll(queryResult.getResult());
-                } else {
-                    for (QueryResultEntry queryResultEntry : queryResult.getResult()) {
-                        Object key = ss.toObject(queryResultEntry.getKeyData());
-                        Object value = ss.toObject(queryResultEntry.getValueData());
-                        result.add(new AbstractMap.SimpleImmutableEntry(key, value));
-                    }
-                }
+                result.addAll(queryResult.getResult());
             }
         } catch (Throwable t) {
             throw ExceptionUtil.rethrow(t);
-        }
-        if (pagingPredicate != null) {
-            PagingPredicateAccessor.setPagingPredicateAnchor(pagingPredicate, ((SortedQueryResultSet) result).last());
         }
         return result;
     }
