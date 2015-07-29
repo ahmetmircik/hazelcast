63ad1074c0593d4f9139b917313479edbc6ad20e
Ali Gurbuz <ali@hazelcast.com>
2015-07-29 14:38:28 +0300

    introduced operation-provider

--- hazelcast/src/main/java/com/hazelcast/nio/serialization/impl/FactoryIdHelper.java
+++ hazelcast/src/main/java/com/hazelcast/nio/serialization/impl/FactoryIdHelper.java
@@ -86,6 +86,9 @@ public final class FactoryIdHelper {
     public static final String ENTERPRISE_WAN_REPLICATION_DS_FACTORY = "hazelcast.serialization.ds.enterprise.wan_replication";
     public static final int ENTERPRISE_WAN_REPLICATION_DS_FACTORY_ID = -28;
 
+    public static final String ENTERPRISE_MAP_DS_FACTORY = "hazelcast.serialization.ds.enterprise.map";
+    public static final int ENTERPRISE_MAP_DS_FACTORY_ID = -30;
+
     public static final String RINGBUFFER_DS_FACTORY = "hazelcast.serialization.ds.ringbuffer";
     public static final int RINGBUFFER_DS_FACTORY_ID = -29;
 
