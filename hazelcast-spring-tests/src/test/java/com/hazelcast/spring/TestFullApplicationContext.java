2cdedd596068770e7ec10c8112734a81cc1d9985
dominikrebhan <dominik@home>
2021-01-30 09:26:54 +0100

    Created predefined FIFO-eviction-policy (#18075)

--- hazelcast-spring-tests/src/test/java/com/hazelcast/spring/TestFullApplicationContext.java
+++ hazelcast-spring-tests/src/test/java/com/hazelcast/spring/TestFullApplicationContext.java
@@ -1317,6 +1317,7 @@ public class TestFullApplicationContext extends HazelcastTestSupport {
 
     @Test
     public void testMapEvictionPolicies() {
+        assertEquals(EvictionPolicy.FIFO, config.getMapConfig("fifoEvictionMap").getEvictionConfig().getEvictionPolicy());
         assertEquals(EvictionPolicy.LFU, config.getMapConfig("lfuEvictionMap").getEvictionConfig().getEvictionPolicy());
         assertEquals(EvictionPolicy.LRU, config.getMapConfig("lruEvictionMap").getEvictionConfig().getEvictionPolicy());
         assertEquals(EvictionPolicy.NONE, config.getMapConfig("noneEvictionMap").getEvictionConfig().getEvictionPolicy());
@@ -1325,6 +1326,7 @@ public class TestFullApplicationContext extends HazelcastTestSupport {
 
     @Test
     public void testMemberNearCacheEvictionPolicies() {
+        assertEquals(EvictionPolicy.FIFO, getNearCacheEvictionPolicy("fifoNearCacheEvictionMap", config));
         assertEquals(EvictionPolicy.LFU, getNearCacheEvictionPolicy("lfuNearCacheEvictionMap", config));
         assertEquals(EvictionPolicy.LRU, getNearCacheEvictionPolicy("lruNearCacheEvictionMap", config));
         assertEquals(EvictionPolicy.RANDOM, getNearCacheEvictionPolicy("randomNearCacheEvictionMap", config));
